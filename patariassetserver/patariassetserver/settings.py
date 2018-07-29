4df282674230ff1bb155513833703de60e0c6804
htalat <hassi.talat@gmail.com>
2018-07-29 14:55:36 +0500

    remove a redundant statement and add cors middleware

--- patariassetserver/patariassetserver/settings.py
+++ patariassetserver/patariassetserver/settings.py
@@ -37,18 +37,25 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'assetserver'
+    'assetserver',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_WHITELIST = (
+    'localhost:8800',
+    '127.0.0.1',
+)
 
 TRANSFER_SERVER = 'nginx'  # From 'apache', 'nginx', 'lighttpd'
 
